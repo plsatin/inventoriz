5c3af51b7699b741fc260ce244d3002bedca298f
Павел Сатин <it4@rezhcable.ru>
2021-10-25 15:41:45 +0500

    v2

--- database/migrations/2021_10_25_060449_create_wmiproperties_table.php
+++ database/migrations/2021_10_25_060449_create_wmiproperties_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class WmiProperties extends Migration
+class Wmiproperties extends Migration
 {
     /**
      * Run the migrations.
