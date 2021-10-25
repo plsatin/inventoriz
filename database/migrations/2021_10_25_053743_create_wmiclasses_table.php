2ecc864db79b17b4e9674eb959ec3612a98b5de3
Павел Сатин <it4@rezhcable.ru>
2021-10-25 15:43:13 +0500

    v3

--- database/migrations/2021_10_25_053743_create_wmiclasses_table.php
+++ database/migrations/2021_10_25_053743_create_wmiclasses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Wmiclasses extends Migration
+class CreateWmiclassesTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2021_10_25_060449_create_wmiproperties_table.php
+++ database/migrations/2021_10_25_060449_create_wmiproperties_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Wmiproperties extends Migration
+class CreateWmipropertiesTable extends Migration
 {
     /**
      * Run the migrations.
