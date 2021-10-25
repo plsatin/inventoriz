56ab4f1aef8787b43e3da13a4df6f65d7781f84e
Павел Сатин <it4@rezhcable.ru>
2021-10-25 15:37:43 +0500

    rename migrations

--- database/migrations/2021_10_25_062745_create_computers_table.php
+++ database/migrations/2021_10_25_062745_create_computers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateComputersTable extends Migration
+class Computers extends Migration
 {
     /**
      * Run the migrations.
