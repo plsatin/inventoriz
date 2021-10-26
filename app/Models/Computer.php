34c251d3c1a680391d41ecc633308b589d9bbfe3
Павел Сатин <it4@rezhcable.ru>
2021-10-26 06:41:36 +0500

    +2

--- app/Models/Computer.php
+++ app/Models/Computer.php
@@ -26,5 +26,14 @@ class Computer extends Model
 
 
 
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at', 'updated_at',
+    ];
+
 
 }
