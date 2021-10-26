45d5cbefe7172b491ea8628efeca54246c80390d
Павел Сатин <it4@rezhcable.ru>
2021-10-26 11:41:27 +0500

    icons

--- app/Http/Controllers/ComputerPropertiesController.php
+++ app/Http/Controllers/ComputerPropertiesController.php
@@ -83,7 +83,7 @@ class ComputerPropertiesController extends Controller
 
                 }
 
-
+                $computerClasses[$classCount]->icon = '/assets/img/icons/' + $computerClasses[$classCount]->icon
                 $computerClasses[$classCount]->children = $classProperties;
                 $classCount ++;
             }
