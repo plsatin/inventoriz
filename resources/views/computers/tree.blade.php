41a075668f67c7fc9968dca40047008057e58a67
Павел Сатин <it4@rezhcable.ru>
2021-10-26 10:15:07 +0500

    tree

--- resources/views/computers/tree.blade.php
+++ resources/views/computers/tree.blade.php
@@ -18,7 +18,7 @@
 $('#tree').jstree({
     'core': {
         'data': {
-            'url': "/api/v1/computers/6/properties",
+            'url': "/api/v1/computers/6",
             'type': 'GET',
             'dataType': 'JSON',
             'contentType':'application/json',
