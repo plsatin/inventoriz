46a7e179f0080762dffdc35c450303253f392aa1
Павел Сатин <it4@rezhcable.ru>
2021-10-26 09:05:38 +0500

    v6

--- app/Http/Controllers/ComputerPropertiesController.php
+++ app/Http/Controllers/ComputerPropertiesController.php
@@ -58,7 +58,7 @@ class ComputerPropertiesController extends Controller
 
                     $classPropertiesInstance = ComputerProperties::where('computer_id', $computer->id)
                     ->where('computer_properties.wmiclass_id', $class->id)->where('instance_id', $instance->instance_id)
-                        ->join('wmiproperties', 'computer_properties.wmiproperty_id', '=', 'wmiproperties.id')
+                        // ->join('wmiproperties', 'computer_properties.wmiproperty_id', '=', 'wmiproperties.id')
                             ->get();
                     
                     $classProperties[$instanceId]->id = $instance->instance_id;
