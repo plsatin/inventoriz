55ce545a813ad9d5fb81594407e623e4f9fac939
Павел Сатин <it4@rezhcable.ru>
2021-10-28 16:15:51 +0500

    v7

--- resources/views/computers/tree.blade.php
+++ resources/views/computers/tree.blade.php
@@ -10,39 +10,30 @@
 
 <style>
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
-/* Create two unequal columns that floats next to each other */
+/* Create two equal columns that floats next to each other */
 .column {
-    float: left;
-    padding: 10px;
-
-}
-
-.left {
-    width: 25%;
-    min-width: 300px;
-}
-
-.right {
-    width: auto;
-    max-width: 100%;
-}
-
-
-.row {
-  display: flex;
+  float: left;
+  width: 25%;
+  min-width: 300px;
+  padding: 10px;
 }
 
 /* Clear floats after the columns */
 .row:after {
-    content: "";
-    display: table;
-    clear: both;
+  content: "";
+  display: table;
+  clear: both;
 }
 
-</style>
+/* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+  }
+}</style>
 
 
 
