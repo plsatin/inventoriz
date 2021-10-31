0645042dfd2748879480f0915cd8a55cdf663ad1
Павел Сатин <it4@rezhcable.ru>
2021-10-31 09:59:38 +0500

    v11

--- database/migrations/2021_09_031_092825_users.php
+++ database/migrations/2021_09_031_092825_users.php
@@ -8,7 +8,7 @@ class Users extends Migration{
     
     public function up(){
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('role_id')->unsigned();
+            $table->integer('role_id')->unsigned()->default(2);
             $table->foreign('role_id')->references('id')->on('roles');
         });
     }
