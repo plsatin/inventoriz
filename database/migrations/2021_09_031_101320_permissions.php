010d57dcdd11919b22f0000259516d4ebf1da325
Павел Сатин <it4@rezhcable.ru>
2021-10-31 10:14:14 +0500

    Add permissions

---
+++ database/migrations/2021_09_031_101320_permissions.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Permissions extends Migration{
+    
+    public function up(){
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('key')->nullable();
+            $table->string('controller');
+            $table->string('method');
+            $table->timestamps();
+           });
+    }
+
+    public function down(){
+        Schema::dropIfExists('permissions');
+    }
+}
