079c6fc95fd23952ab052859ed28490ba5bd62b5
Павел Сатин <it4@rezhcable.ru>
2021-11-03 06:37:01 +0500

    computers mig

---
+++ database/migrations/2021_11_03_055618_computers.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Computers extends Migration
+{
+    
+/** Добавляем время начала и конца инвентаризации. Добавляем счетчик инвентаризаций */
+
+
+    public function up()
+    {
+        Schema::table('computers', function (Blueprint $table) {
+            $table->dateTime('last_inventory_start')->nullable();
+            $table->dateTime('last_inventory_end')->nullable();
+            $table->bigInteger('last_inventory_index')->nullable();
+
+            $table->dropColumn('last_inventory_report');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('computers', function (Blueprint $table) {
+            $table->dateTime('last_inventory_report')->nullable();
+
+            $table->dropColumn('last_inventory_start');
+            $table->dropColumn('last_inventory_end');
+            $table->dropColumn('last_inventory_index');
+        });
+    }
+}
