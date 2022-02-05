d7500a7870df4ee4efb8ff1ddf7b8ed7386e6d47
Павел Сатин <it4@rezhcable.ru>
2022-02-05 13:45:53 +0500

    404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,16 +1,30 @@
 @extends('layouts.app')
 @section('title')
-<div class="page-header">
-  <h1>{{ $page_title ?? '' }}</h1>
-</div>
+{{-- <div class="page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-24">
+                <h2>{{ $page_title ?? '' }}</h2>
+            </div>
+        </div>
+    </div>
+</div> --}}
 @endsection
 @section('content')
 
-
-<div class="page-content-404">
-<h2>404</h2>
-<p>Страница не найдена</p>
-
+<div class="container">
+    <div class="row">
+        <div class="col-xs-24">
+            <section class="section">
+                <header class="section-header">
+                    <h1 class="section-title">
+                        404
+                    </h1>
+                </header>
+                <p>Страница не найдена</p>
+            </section>
+        </div>
+    </div>
 </div>
 
 @endsection
