fd9b1cb09fb5448d9469626bfb9d15b0fe52cd48
Павел Сатин <it4@rezhcable.ru>
2022-02-05 15:25:36 +0500

    м4

--- resources/views/computers/tree-list.blade.php
+++ resources/views/computers/tree-list.blade.php
@@ -136,7 +136,7 @@ var computerId;
                         '<div class="content-text-primary" style="cursor: pointer;">' +
                             '<span id="computer-id_' + data[computer].id + '">' + data[computer].name + '</span>' +
                         '</div>' +
-                        '<div class="content-text-secondary">Последний опрос: ' + data[computer].last_inventory_start + '</div>' +
+                        '<div class="content-text-secondary"><span class="type-t9">Состояние на: ' + data[computer].last_inventory_start + '</span></div>' +
                     '</div>' +
                 '</div>';
             }
