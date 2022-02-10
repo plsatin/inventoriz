91750ee72b2d91bd35deaa5506d507f5d012ce55
Павел Сатин <it4@rezhcable.ru>
2022-02-10 20:21:42 +0500

    v71

--- resources/views/reports/charts.blade.php
+++ resources/views/reports/charts.blade.php
@@ -188,13 +188,6 @@
             vAxis: {minValue: 0},
             curveType: 'function',
             legend: { position: 'bottom' },
-            chartArea: {
-                left: 0,
-                height: 250,
-                width: 400
-            },
-            height: 300,
-            width: 600,
 
         };
 
