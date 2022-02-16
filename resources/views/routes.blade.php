ad278461e626dbb402db68236c9ea5813bcf1d22
Павел Сатин <it4@rezhcable.ru>
2022-02-16 17:28:17 +0500

    container

--- resources/views/routes.blade.php
+++ resources/views/routes.blade.php
@@ -13,28 +13,48 @@
 @section('content')
 
 
+<div class="container">
+    <div class="row">
+        <div class="col-xs-24">
+            <section class="section">
+                <header class="section-header">
+                    <h1 class="section-title">
+                        {{ $page_title ?? '' }}
+                    </h1>
+                </header>
+    
+                <div class="row">
+                    <div class="col-xs-24">
 
+                        <table id="routes-table" class="table table-bordered table-responsive">
+                            <thead>
+                                    <tr>
+                                        <th>uri</th>
+                                        <th>Controller</th>
+                                        <th>Action</th>
+                                        <th>Method</th>
+                                    </tr>
+                            </thead>
+                            <tbody>
+                                    @foreach ($routes as $route )
+                                        <tr>
+                                            <td>{{ $route->uri }}</td>
+                                            <td>{{ $route->controller }}</td>
+                                            <td>{{ $route->action }}</td>
+                                            <td>{{ $route->method }}</td>
+                                        </tr>
+                                    @endforeach
+                            </tbody>
+                        </table>
 
-<table id="routes-table" class="table table-bordered table-responsive">
-    <thead>
-             <tr>
-                 <th>uri</th>
-                 <th>Controller</th>
-                 <th>Action</th>
-                 <th>Method</th>
-             </tr>
-    </thead>
-    <tbody>
-             @foreach ($routes as $route )
-                 <tr>
-                     <td>{{ $route->uri }}</td>
-                     <td>{{ $route->controller }}</td>
-                     <td>{{ $route->action }}</td>
-                     <td>{{ $route->method }}</td>
-                 </tr>
-             @endforeach
-     </tbody>
-</table>
+                    </div>
+                </div>
+            
+            </section>
+        </div>
+    </div>
+</div>
+            
 
 
 @endsection
