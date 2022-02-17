18fa36f6058bd2487d485f762777b51f4eff3fcf
Павел Сатин <it4@rezhcable.ru>
2022-02-17 09:52:37 +0500

    software

--- resources/views/computers/tree.blade.php
+++ resources/views/computers/tree.blade.php
@@ -63,7 +63,7 @@
                 iconTooltip: function(event, data) {
                     return data.typeInfo.iconTooltip;
                 },
-                source: {url: '/api/v1/computers/'+computerId+'/properties'},
+                source: {url: '/api/v1/computers/'+computerId+'/software'},
             });
         }
 
