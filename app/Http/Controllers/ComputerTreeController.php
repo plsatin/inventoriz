b75523b01e9924f702c689126e213fcc2c4a3b76
Павел Сатин <it4@rezhcable.ru>
2022-02-20 11:45:38 +0500

    v34456

--- app/Http/Controllers/ComputerTreeController.php
+++ app/Http/Controllers/ComputerTreeController.php
@@ -38,7 +38,7 @@ class ComputerTreeController extends Controller
             $computerName = $request->get('name');
             $computer = Computer::where('name', $computerName)->firstOrFail();
 
-            return response()->json($computer, 200);
+            return response()->json($computer);
         } catch (Exception $e) {
             $responseObject = array('Response' => 'Error', 'data' => array('Code' => $e->getCode(), 'Message' => $e->getMessage()));
             return response()->json($responseObject, 201);
