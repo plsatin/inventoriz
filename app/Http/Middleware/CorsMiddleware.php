a23ef8b5ca9d453c8d15c2c7e41abe29cfc0a6f7
Павел Сатин <it4@rezhcable.ru>
2022-02-21 22:01:48 +0500

    v0.2

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,12 +15,39 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
+
+        // // Версия 0.1
+        // $response = $next($request);
+
+        // $response->header('Access-Control-Allow-Origin','*');
+
+        // // Версия 0.1.1
+        // $response->header('Access-Control-Allow-Headers','Content-Type,Authorization');
+        // $response->header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS');
+
+        // return $response;
+
+
+        // Версия 0.2
+        header("Access-Control-Allow-Origin: *");
+
+        // ALLOW OPTIONS METHOD
+        $headers = [
+            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers'=> 'Content-Type, Authorization, Origin'
+        ];
+        if($request->getMethod() == "OPTIONS") {
+            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+            return Response::make('OK', 200, $headers);
+        }
+
         $response = $next($request);
-
-        $response->header('Access-Control-Allow-Origin','*');
-        $response->header('Access-Control-Allow-Headers','Content-Type,Authorization');
-        $response->header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS');
-
+        foreach($headers as $key => $value)
+            $response->header($key, $value);
         return $response;
+
     }
 }
+
+
+
